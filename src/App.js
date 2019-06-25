dcd97af57f8146be484fbed11c2db7d873b4a7f0
fullstackmafia <ugwuraphael@gmail.com>
2019-06-25 09:20:39 +0100

    good

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      info: [],
+    }
+  };
+
+  componentDidMount() {
+    const API = "5327f1d2dd284b90914d421702c5ca9c";
+    fetch(`http://api1.5sim.net/stubs/handler_api.php?api_key=${API}&action=getBalance`)
+    .then(response => {
+      console.log(response);
+      return response.json;
+    }).then(data => {
+      console.log(data)
+    }) 
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>Good</h4>
+      </div>
+    )
+  }
 }
 
 export default App;
